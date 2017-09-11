66291da05140f118200949aae0abe76ea2704041
yuemenglong <yuemenglong@126.com>
2017-09-11 10:06:13 +0800

    add: finish twinsornot

--- src/main/java/io/github/yuemenglong/http/HttpClient.scala
+++ src/main/java/io/github/yuemenglong/http/HttpClient.scala
@@ -117,34 +117,3 @@ class HttpClient {
     generateResponse(response)
   }
 }
-
-object Main {
-
-  def uploadFile(path: String, lr: String): String = {
-    val client = new HttpClient
-    val file = Main.getClass.getClassLoader.getResource(path).getFile
-    val url = s"https://www.twinsornot.net/Home/AnalyzeOneImage?isTest=False&fileid=$lr"
-    val res = client.httpForm(url, Map(s"processedImg$lr" -> new File(file)))
-    println(res)
-    val json = res.getBody.replaceAll("\\\\\"", "\"").replaceAll("(\"$)|(^\")", "")
-    val jo = JSON.parse(json).asObj()
-    jo.getArr("Faces").array(0).asObj().getStr("faceId")
-  }
-
-  def similar(left: String, right: String): Int = {
-    val client = new HttpClient
-    val url = s"https://www.twinsornot.net/Home/HowSimilar"
-    val res = client.httpForm(url, Map("leftFaceID" -> left, "rightFaceID" -> right))
-    println(res)
-    val json = res.getBody.replaceAll("\\\\\"", "\"").replaceAll("(\"$)|(^\")", "")
-    val jo = JSON.parse(json).asObj()
-    jo.getInt("Score")
-  }
-
-  def main(args: Array[String]): Unit = {
-    val left = uploadFile("test.jpg", "L")
-    val right = uploadFile("test2.jpg", "R")
-    val score = similar(left, right)
-    println(score)
-  }
-}
