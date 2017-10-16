15823f15e26b7196ccf35ed310e33c7254fa9569
yuemenglong <yuemenglong@126.com>
2017-10-16 17:50:16 +0800

    add: face++

--- src/test/java/FacePlusPlus.scala
+++ src/test/java/FacePlusPlus.scala
@@ -1,6 +1,7 @@
 import java.io.File
 
 import io.github.yuemenglong.http.HttpClient
+import io.github.yuemenglong.json.JSON
 
 /**
   * Created by <yuemenglong@126.com> on 2017/10/16.
@@ -10,7 +11,7 @@ import io.github.yuemenglong.http.HttpClient
 object FacePlusPlus {
 
   def detect(): Unit = {
-    val path = Thread.currentThread().getContextClassLoader.getResource("test.jpg").getFile
+    val path = Thread.currentThread().getContextClassLoader.getResource("test4.jpg").getFile
     println(path)
 
     val client = new HttpClient
@@ -18,17 +19,14 @@ object FacePlusPlus {
       "api_key" -> "uqngbdsbwX9CsbqPeObfwzzlaUJpPDJC",
       "api_secret" -> "Q5kEO5lhl32wvb3mMhY0AIu90nEAob1o",
       "image_file" -> new File(path),
-      //      "return_landmark" -> 1,
-      "return_attributes" -> "gender,age",
+      "return_landmark" -> 1,
+      "return_attributes" -> "gender,age,smiling,headpose,facequality,blur,eyestatus,emotion,ethnicity,beauty,mouthstatus,eyegaze,skinstatus",
     )
     val res = client.httpForm("https://api-cn.faceplusplus.com/facepp/v3/detect", formData)
     println(res.getBody)
   }
 
-  def compare(): Unit = {
-    val path1 = Thread.currentThread().getContextClassLoader.getResource("test.jpg").getFile
-    val path2 = Thread.currentThread().getContextClassLoader.getResource("test4.jpg").getFile
-
+  def compare(path1: String, path2: String): Double = {
     val client = new HttpClient
     val formData = Map[String, Any](
       "api_key" -> "uqngbdsbwX9CsbqPeObfwzzlaUJpPDJC",
@@ -37,11 +35,18 @@ object FacePlusPlus {
       "image_file2" -> new File(path2),
     )
     val res = client.httpForm("https://api-cn.faceplusplus.com/facepp/v3/compare", formData)
-    println(res.getBody)
+    JSON.parse(res.getBody).asObj().getDouble("confidence")
   }
 
   def main(args: Array[String]): Unit = {
-    compare()
+    detect()
+    //    3.to(5).foreach(i => {
+    //      (i + 1).to(6).foreach(j => {
+    //        val path1 = Thread.currentThread().getContextClassLoader.getResource(s"test$i.jpg").getFile
+    //        val path2 = Thread.currentThread().getContextClassLoader.getResource(s"test$j.jpg").getFile
+    //        println(i, j, compare(path1, path2))
+    //      })
+    //    })
   }
 
 }
---
+++ src/test/resources/detect-res3.js
@@ -0,0 +1,556 @@
+detectRes = {
+    "image_id": "RHeNugLDt1oq2oIsBnRVFw==",
+    "request_id": "1508145319,84400b01-3e8c-49e6-8987-93a22fabb5e9",
+    "time_used": 477,
+    "faces": [
+    {
+        "landmark":
+        {
+            "mouth_upper_lip_left_contour2":
+            {
+                "y": 281,
+                "x": 518
+            },
+            "mouth_upper_lip_top":
+            {
+                "y": 280,
+                "x": 541
+            },
+            "mouth_upper_lip_left_contour1":
+            {
+                "y": 279,
+                "x": 532
+            },
+            "left_eye_upper_left_quarter":
+            {
+                "y": 190,
+                "x": 484
+            },
+            "left_eyebrow_lower_middle":
+            {
+                "y": 171,
+                "x": 489
+            },
+            "mouth_upper_lip_left_contour3":
+            {
+                "y": 285,
+                "x": 523
+            },
+            "left_eyebrow_lower_left_quarter":
+            {
+                "y": 172,
+                "x": 473
+            },
+            "right_eyebrow_lower_left_quarter":
+            {
+                "y": 167,
+                "x": 573
+            },
+            "right_eye_pupil":
+            {
+                "y": 189,
+                "x": 580
+            },
+            "mouth_lower_lip_right_contour1":
+            {
+                "y": 285,
+                "x": 561
+            },
+            "mouth_lower_lip_left_contour2":
+            {
+                "y": 292,
+                "x": 515
+            },
+            "mouth_lower_lip_right_contour3":
+            {
+                "y": 297,
+                "x": 559
+            },
+            "mouth_lower_lip_right_contour2":
+            {
+                "y": 288,
+                "x": 571
+            },
+            "contour_chin":
+            {
+                "y": 342,
+                "x": 546
+            },
+            "contour_left9":
+            {
+                "y": 339,
+                "x": 522
+            },
+            "left_eye_lower_right_quarter":
+            {
+                "y": 200,
+                "x": 504
+            },
+            "mouth_lower_lip_top":
+            {
+                "y": 289,
+                "x": 542
+            },
+            "right_eyebrow_upper_middle":
+            {
+                "y": 155,
+                "x": 587
+            },
+            "right_eyebrow_left_corner":
+            {
+                "y": 168,
+                "x": 558
+            },
+            "right_eye_lower_right_quarter":
+            {
+                "y": 194,
+                "x": 592
+            },
+            "right_eye_bottom":
+            {
+                "y": 196,
+                "x": 583
+            },
+            "contour_left7":
+            {
+                "y": 315,
+                "x": 485
+            },
+            "contour_left6":
+            {
+                "y": 299,
+                "x": 470
+            },
+            "contour_left5":
+            {
+                "y": 281,
+                "x": 460
+            },
+            "contour_left4":
+            {
+                "y": 260,
+                "x": 453
+            },
+            "contour_left3":
+            {
+                "y": 239,
+                "x": 449
+            },
+            "contour_left2":
+            {
+                "y": 218,
+                "x": 447
+            },
+            "contour_left1":
+            {
+                "y": 197,
+                "x": 446
+            },
+            "left_eye_lower_left_quarter":
+            {
+                "y": 198,
+                "x": 484
+            },
+            "contour_right1":
+            {
+                "y": 190,
+                "x": 624
+            },
+            "contour_right3":
+            {
+                "y": 230,
+                "x": 624
+            },
+            "contour_right2":
+            {
+                "y": 210,
+                "x": 625
+            },
+            "mouth_left_corner":
+            {
+                "y": 282,
+                "x": 504
+            },
+            "contour_right4":
+            {
+                "y": 250,
+                "x": 621
+            },
+            "contour_right7":
+            {
+                "y": 305,
+                "x": 596
+            },
+            "left_eyebrow_left_corner":
+            {
+                "y": 174,
+                "x": 459
+            },
+            "nose_right":
+            {
+                "y": 249,
+                "x": 567
+            },
+            "nose_tip":
+            {
+                "y": 245,
+                "x": 538
+            },
+            "contour_right5":
+            {
+                "y": 270,
+                "x": 616
+            },
+            "nose_contour_lower_middle":
+            {
+                "y": 261,
+                "x": 540
+            },
+            "right_eye_top":
+            {
+                "y": 183,
+                "x": 581
+            },
+            "mouth_lower_lip_left_contour3":
+            {
+                "y": 299,
+                "x": 527
+            },
+            "right_eye_right_corner":
+            {
+                "y": 190,
+                "x": 600
+            },
+            "mouth_upper_lip_right_contour1":
+            {
+                "y": 277,
+                "x": 550
+            },
+            "mouth_upper_lip_right_contour2":
+            {
+                "y": 277,
+                "x": 565
+            },
+            "right_eyebrow_lower_right_quarter":
+            {
+                "y": 166,
+                "x": 601
+            },
+            "left_eye_left_corner":
+            {
+                "y": 195,
+                "x": 475
+            },
+            "mouth_right_corner":
+            {
+                "y": 277,
+                "x": 581
+            },
+            "right_eye_lower_left_quarter":
+            {
+                "y": 196,
+                "x": 573
+            },
+            "left_eyebrow_right_corner":
+            {
+                "y": 173,
+                "x": 521
+            },
+            "left_eyebrow_lower_right_quarter":
+            {
+                "y": 173,
+                "x": 505
+            },
+            "right_eye_center":
+            {
+                "y": 191,
+                "x": 582
+            },
+            "left_eye_pupil":
+            {
+                "y": 194,
+                "x": 496
+            },
+            "nose_left":
+            {
+                "y": 252,
+                "x": 514
+            },
+            "mouth_lower_lip_left_contour1":
+            {
+                "y": 288,
+                "x": 523
+            },
+            "left_eye_upper_right_quarter":
+            {
+                "y": 191,
+                "x": 506
+            },
+            "right_eyebrow_lower_middle":
+            {
+                "y": 166,
+                "x": 587
+            },
+            "left_eye_center":
+            {
+                "y": 195,
+                "x": 494
+            },
+            "contour_left8":
+            {
+                "y": 329,
+                "x": 501
+            },
+            "contour_right9":
+            {
+                "y": 335,
+                "x": 567
+            },
+            "right_eye_left_corner":
+            {
+                "y": 195,
+                "x": 564
+            },
+            "left_eyebrow_upper_left_quarter":
+            {
+                "y": 162,
+                "x": 472
+            },
+            "left_eye_bottom":
+            {
+                "y": 200,
+                "x": 494
+            },
+            "left_eye_right_corner":
+            {
+                "y": 198,
+                "x": 514
+            },
+            "right_eyebrow_upper_left_quarter":
+            {
+                "y": 158,
+                "x": 571
+            },
+            "contour_right8":
+            {
+                "y": 321,
+                "x": 583
+            },
+            "right_eyebrow_right_corner":
+            {
+                "y": 167,
+                "x": 614
+            },
+            "right_eye_upper_left_quarter":
+            {
+                "y": 187,
+                "x": 571
+            },
+            "left_eyebrow_upper_middle":
+            {
+                "y": 160,
+                "x": 489
+            },
+            "right_eyebrow_upper_right_quarter":
+            {
+                "y": 157,
+                "x": 602
+            },
+            "nose_contour_left1":
+            {
+                "y": 197,
+                "x": 525
+            },
+            "nose_contour_left2":
+            {
+                "y": 235,
+                "x": 519
+            },
+            "nose_contour_left3":
+            {
+                "y": 258,
+                "x": 526
+            },
+            "nose_contour_right1":
+            {
+                "y": 196,
+                "x": 552
+            },
+            "nose_contour_right2":
+            {
+                "y": 234,
+                "x": 560
+            },
+            "mouth_lower_lip_bottom":
+            {
+                "y": 301,
+                "x": 543
+            },
+            "contour_right6":
+            {
+                "y": 289,
+                "x": 607
+            },
+            "nose_contour_right3":
+            {
+                "y": 256,
+                "x": 554
+            },
+            "left_eye_top":
+            {
+                "y": 188,
+                "x": 495
+            },
+            "mouth_upper_lip_right_contour3":
+            {
+                "y": 283,
+                "x": 561
+            },
+            "left_eyebrow_upper_right_quarter":
+            {
+                "y": 163,
+                "x": 506
+            },
+            "right_eye_upper_right_quarter":
+            {
+                "y": 185,
+                "x": 591
+            },
+            "mouth_upper_lip_bottom":
+            {
+                "y": 287,
+                "x": 542
+            }
+        },
+        "attributes":
+        {
+            "emotion":
+            {
+                "sadness": 36.307,
+                "neutral": 0.623,
+                "disgust": 0.061,
+                "anger": 0.018,
+                "surprise": 5.308,
+                "fear": 0.018,
+                "happiness": 57.666
+            },
+            "beauty":
+            {
+                "female_score": 84.679,
+                "male_score": 81.119
+            },
+            "gender":
+            {
+                "value": "Female"
+            },
+            "age":
+            {
+                "value": 26
+            },
+            "mouthstatus":
+            {
+                "close": 99.991,
+                "surgical_mask_or_respirator": 0.0,
+                "open": 0.005,
+                "other_occlusion": 0.004
+            },
+            "glass":
+            {
+                "value": "None"
+            },
+            "skinstatus":
+            {
+                "dark_circle": 4.696,
+                "stain": 11.049,
+                "acne": 6.882,
+                "health": 60.833
+            },
+            "headpose":
+            {
+                "yaw_angle": -1.8244954,
+                "pitch_angle": 7.319842,
+                "roll_angle": -2.045699
+            },
+            "blur":
+            {
+                "blurness":
+                {
+                    "threshold": 50.0,
+                    "value": 1.041
+                },
+                "motionblur":
+                {
+                    "threshold": 50.0,
+                    "value": 1.041
+                },
+                "gaussianblur":
+                {
+                    "threshold": 50.0,
+                    "value": 1.041
+                }
+            },
+            "smile":
+            {
+                "threshold": 30.1,
+                "value": 68.147
+            },
+            "eyestatus":
+            {
+                "left_eye_status":
+                {
+                    "normal_glass_eye_open": 0.301,
+                    "no_glass_eye_close": 0.0,
+                    "occlusion": 0.001,
+                    "no_glass_eye_open": 99.697,
+                    "normal_glass_eye_close": 0.001,
+                    "dark_glasses": 0.001
+                },
+                "right_eye_status":
+                {
+                    "normal_glass_eye_open": 0.002,
+                    "no_glass_eye_close": 0.0,
+                    "occlusion": 0.0,
+                    "no_glass_eye_open": 99.997,
+                    "normal_glass_eye_close": 0.0,
+                    "dark_glasses": 0.0
+                }
+            },
+            "facequality":
+            {
+                "threshold": 70.1,
+                "value": 91.918
+            },
+            "ethnicity":
+            {
+                "value": "Asian"
+            },
+            "eyegaze":
+            {
+                "right_eye_gaze":
+                {
+                    "position_x_coordinate": 0.488,
+                    "vector_z_component": 0.997,
+                    "vector_x_component": -0.077,
+                    "vector_y_component": -0.013,
+                    "position_y_coordinate": 0.435
+                },
+                "left_eye_gaze":
+                {
+                    "position_x_coordinate": 0.537,
+                    "vector_z_component": 0.981,
+                    "vector_x_component": 0.183,
+                    "vector_y_component": -0.064,
+                    "position_y_coordinate": 0.396
+                }
+            }
+        },
+        "face_rectangle":
+        {
+            "width": 185,
+            "top": 157,
+            "left": 444,
+            "height": 185
+        },
+        "face_token": "66d1a5231953ead24c7cbc7096284c3d"
+    }]
+}
